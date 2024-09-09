873c4bf5e3d8d9e5cf15ef23095366067649f29c
nasrullah-mansur <nasrullah.cit.bd@gmail.com>
2024-09-09 21:37:50 +0600

    html css done with responsive

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,13 @@
 
 
 
+let menuToggle = document.getElementById('menu-toggle');
+let menuList = document.getElementById('menu-list');
+
+menuToggle.addEventListener('click', function() {
+    menuList.classList.toggle('active');    
+});
+
+
 
 
