fc128a6c9cdb80b8d10f62ccf5496cc726674ad7
nasrullah-mansur <nasrullah.cit.bd@gmail.com>
2024-09-22 20:41:04 +0600

    food items fetching done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,5 @@
+init();
+
 function menu_responsive() {
     let menuToggle = document.getElementById('menu-toggle');
     let menuList = document.getElementById('menu-list');
@@ -7,13 +9,63 @@ function menu_responsive() {
     });
 }
 
-menu_responsive();
+// ****************** Function Init **************************
+function init() {
+    menu_responsive();
+    foodCategories();
+    randomFoods();
+}
+
+// ****************** Helper Functions **************************
+function foodCart(food) {
+    return `<div class="col-lg-4 col-md-6 mb-4">
+            <div class="food-cart-item">
+                <div class="img position-relative">
+                    <img class="img-fluid w-100" src="${food.image}" alt="img">
+                    <div class="overlay position-absolute d-flex align-items-center">
+                        <span>Price: ${food.price}/=</span>
+                        <i class="fa-solid fa-star"></i>
+                        <span> ${food.rating} (${food.rating_count})</span>
+                    </div>
+                </div>
+                <div class="text">
+                    <h4 class="pb-2">${food.name}</h4>
+                    <ul class="list-unstyled d-flex flex-wrap">
+                        <li class="position-relative">4 chicken legs</li>
+                        <li class="position-relative">Chili sauce</li>
+                        <li class="position-relative">4 chicken legs</li>
+                        <li class="position-relative">Soft Drinks</li>
+                        <li class="position-relative">Soft Drinks</li>
+                        <li class="position-relative">Chili sauce</li>
+                    </ul>
+                </div>
+                <a href="#" class="add-to-cart d-flex justify-content-center align-items-center text-decoration-none">
+                    <i class="fa-solid fa-cart-plus"></i>
+                    <span class="d-inline-block">Add to cart</span>
+                </a>
+            </div>
+        </div>`;
+}
+
+async function appendFoodItemsToHtml(link) {
+    try {
+        let response = await fetch(link);
+        let data = await response.json();
+        document.getElementById('food_gallery').innerHTML = '';
+
+        data = Array.isArray(data) ? data : data.data;
+
+        let finalOutput = data.map(food => {
+            document.getElementById('food_gallery').innerHTML += foodCart(food);
+        });
 
 
-// Food menus;
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-foodCategories();
-
+// ****************** Handler Functions **************************
 async function foodCategories() {
 
     try {
@@ -23,13 +75,33 @@ async function foodCategories() {
         document.getElementById('category_list').innerHTML = '';
 
         data.forEach((item) => {
-            document.getElementById('category_list').innerHTML += `<li><a class="text-decoration-none d-inline-block text-uppercase" href="#">${item.name}</a></li>`;
+            document.getElementById('category_list').innerHTML += `<li data-id="${item.id}"><a class="text-decoration-none d-inline-block text-uppercase" href="#">${item.name}</a></li>`;
         })
+
+        foodItemsByCategory();
 
     } catch (error) {
         console.log(error);
     }
-
-
     
 }
+
+function randomFoods() {
+    appendFoodItemsToHtml("https://course.divinecoder.com/food/random/6");
+}
+
+async function foodItemsByCategory() {
+    let lis = document.querySelectorAll('#category_list li');
+    
+    let finalLiList = Array.from(lis).map(li => {
+        li.addEventListener('click', function(event) {
+            event.preventDefault();
+            let categoryId = li.getAttribute('data-id');
+
+            appendFoodItemsToHtml(`https://course.divinecoder.com/food/by-category/${categoryId}/6`);
+            
+        })
+    })
+
+}
+
